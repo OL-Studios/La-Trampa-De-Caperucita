9879ad9b9a462dd388186579e86e813b9ae71b5a
AIID3N <jhon97@hotmail.es>
2024-10-03 00:10:05 -0500

    [Audio firts implmentation in unity & fix bugs environment & test environment]

--- Assets/Scripts/Jugador/JugadorMovimiento.cs
+++ Assets/Scripts/Jugador/JugadorMovimiento.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 namespace Jugador
 {
@@ -21,7 +22,17 @@ namespace Jugador
             public LayerMask mascaraSuelo;
             private bool enSuelo;
             private bool estaRodando = false;
-       #endregion
+
+        [Header("FMOD")]  // Nueva sección para FMOD
+        [EventRef] public string sonidoPasos;  // Aquí va el path del evento de los pasos en FMOD
+        private FMOD.Studio.EventInstance instanciaSonidoPasos;
+        #endregion
+
+        private void Start()
+        {
+            // Crea la instancia del sonido de los pasos
+            instanciaSonidoPasos = RuntimeManager.CreateInstance(sonidoPasos);
+        }
 
         void Update()
         {
@@ -43,7 +54,22 @@ namespace Jugador
             transform.Translate(0, 0, ejeY * Time.deltaTime * velCorrer);
             animatorJugador.SetFloat("velGirar", ejeX);
             animatorJugador.SetFloat("velCorrer", ejeY);
-        }
+
+            if ((ejeX != 0 || ejeY != 0) && enSuelo)
+            {
+                FMOD.Studio.PLAYBACK_STATE estado;
+                instanciaSonidoPasos.getPlaybackState(out estado);
+
+                if (estado != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+                {
+                    instanciaSonidoPasos.start();
+                }
+            }
+            else
+            {
+                instanciaSonidoPasos.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+            }
+    }
 
         void SaltoJugador(){
             enSuelo = Physics.CheckSphere(comprobacionSuelo.position, distanciaSuelo, mascaraSuelo);
@@ -68,6 +94,12 @@ namespace Jugador
             yield return new WaitUntil(() => animatorJugador.GetCurrentAnimatorStateInfo(0).IsName(nombreAnimacion));   // Esperar a que comience la animación
             yield return new WaitWhile(() => animatorJugador.GetCurrentAnimatorStateInfo(0).IsName(nombreAnimacion));   // Esperar a que la animación termine
             estaRodando = false;  // Volver a habilitar el movimiento
+        }
+
+        private void OnDestroy()
+        {
+            // Asegurarse de liberar la instancia de sonido cuando el objeto se destruya
+            instanciaSonidoPasos.release();
         }
     }
 }
